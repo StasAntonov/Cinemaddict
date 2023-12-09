c638b566d652f030cf9a5859fc7a9a24caf62719
IhorPashkovskyi <igorpash96@gmail.com>
2023-12-09 20:05:04 +0200

    [MOV-12] fixed refresh first item size

--- app/src/main/java/com/example/cinemaddict/util/CarouselLayoutManager.kt
+++ app/src/main/java/com/example/cinemaddict/util/CarouselLayoutManager.kt
@@ -25,30 +25,53 @@ class CarouselLayoutManager @JvmOverloads constructor(
         scrollHorizontallyBy(0, recycler, state)
     }
 
+    override fun onLayoutCompleted(state: RecyclerView.State?) =
+        super.onLayoutCompleted(state).also { if (orientation == HORIZONTAL) scaleChildren() }
+
     override fun scrollHorizontallyBy(
         dx: Int,
-        recycler: RecyclerView.Recycler?,
-        state: RecyclerView.State?
-    ): Int {
-        return if (orientation == HORIZONTAL) {
-            for (i in 0 until childCount) {
-                getChildAt(i)?.let { child ->
-                    val startShrinkMidpoint = midpoint - (child.width / 2 + startShrink)
-                    val endShrinkMidpoint = midpoint + (child.width / 2 - endShrink)
-                    val shrinkDistance = endShrinkMidpoint - startShrinkMidpoint
-                    val shrinkPerPixel = (1 - shrinkRatio) / shrinkDistance
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State
+    ) = super.scrollHorizontallyBy(dx, recycler, state).also {
+        if (orientation == HORIZONTAL) scaleChildren()
+    }
 
-                    val itemMidpoint =
-                        abs(((getDecoratedLeft(child) + getDecoratedRight(child)) / 2f) - midpoint)
-                    if (itemMidpoint !in startShrinkMidpoint..endShrinkMidpoint) return@let
-                    val scale =
-                        1f.coerceAtMost(1 - (itemMidpoint - startShrinkMidpoint) * shrinkPerPixel)
-                    child.scaleY = scale
+    private fun scaleChildren() {
+        var translationXForward = 0f
+
+        for (i in 0 until childCount) {
+            getChildAt(i)?.let { child ->
+                val startShrinkMidpoint = midpoint - (child.width / 2 + startShrink)
+                val endShrinkMidpoint = midpoint + (child.width / 2 - endShrink)
+                val shrinkDistance = endShrinkMidpoint - startShrinkMidpoint
+                val shrinkPerPixel = (1 - shrinkRatio) / shrinkDistance
+
+                val childMidpoint = (child.left + child.right) / 2f
+                val distanceToCenter = abs(childMidpoint - midpoint)
+
+                child.isActivated = distanceToCenter < startShrinkMidpoint
+
+                val scale =
+                    1f.coerceAtMost(1 - (distanceToCenter - startShrinkMidpoint) * shrinkPerPixel)
+
+                child.scaleX = scale
+                child.scaleY = scale
+
+                val translationDirection = if (childMidpoint > midpoint) -1 else 1
+                val translationXFromScale = translationDirection * child.width * (1 - scale) / 2f
+                child.translationX = translationXFromScale + translationXForward
+
+                translationXForward = 0f
+
+                if (translationXFromScale > 0 && i >= 1) {
+                    // Edit previous child
+                    getChildAt(i - 1)!!.translationX += 2 * translationXFromScale
+
+                } else if (translationXFromScale < 0) {
+                    // Pass on to next child
+                    translationXForward = 2 * translationXFromScale
                 }
             }
-            super.scrollHorizontallyBy(dx, recycler, state)
-        } else {
-            0
         }
     }
 
