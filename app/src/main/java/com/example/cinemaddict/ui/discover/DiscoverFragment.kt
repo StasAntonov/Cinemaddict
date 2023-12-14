b42ee28d35c81ae5298e9dfc41fc9aebd59d4b61
stanislav <andropa07stas@gmail.com>
2023-12-14 11:15:04 +0200

    [MOV-18] added the query logic

--- app/src/main/java/com/example/cinemaddict/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/example/cinemaddict/ui/discover/DiscoverFragment.kt
@@ -1,28 +1,73 @@
 package com.example.cinemaddict.ui.discover
 
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.cinemaddict.common.paging.MovPagingAdapter
 import com.example.cinemaddict.databinding.FragmentDiscoverBinding
+import com.example.cinemaddict.databinding.ItemDiscoverScreenBinding
+import com.example.cinemaddict.domain.entity.FilmDiscoverData
 import com.example.cinemaddict.ext.toast
 import com.example.cinemaddict.ui.base.BaseUiFragment
 import com.example.cinemaddict.ui.discover.adapter.GenrePagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DiscoverFragment : BaseUiFragment<FragmentDiscoverBinding>(FragmentDiscoverBinding::inflate) {
 
     private lateinit var adapter: GenrePagerAdapter
-
     override val viewModel: DiscoverViewModel by viewModels()
+    private val movieAdapter: MovPagingAdapter<FilmDiscoverData, ItemDiscoverScreenBinding> by lazy {
+        MovPagingAdapter(
+            bindingInflater = ItemDiscoverScreenBinding::inflate,
+            onClickListener = ::navigateToDetailScreen
+        )
+    }
+
+    override fun initViews() {
+        super.initViews()
+        binding.viewModel = viewModel
+        binding.rvList.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            adapter = movieAdapter
+        }
+    }
 
     override fun initListeners() {
         super.initListeners()
         onRefresh(Dispatchers.Main) {
             viewModel.refresh()
         }
+        movieAdapter.addLoadStateListener { loadState ->
+            if (loadState.refresh is LoadState.Loading ||
+                loadState.append is LoadState.Loading
+            ) {
+                showLoader()
+            } else {
+                hideLoader()
+                val errorState = when {
+                    loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                    loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+                    loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                    else -> null
+                }
+                errorState?.let {
+                    toast(it.error.toString())
+                }
+            }
+        }
     }
 
+    @OptIn(FlowPreview::class)
     override fun initObservers() {
         super.initObservers()
         viewModel.genre.observe(viewLifecycleOwner) {
@@ -41,6 +86,43 @@ class DiscoverFragment : BaseUiFragment<FragmentDiscoverBinding>(FragmentDiscove
         viewModel.error.observe(viewLifecycleOwner) {
             toast(it)
         }
+
+        viewModel.searchText.observe(viewLifecycleOwner) {
+            if (!it.isNullOrEmpty()) {
+                isGetMovie(true)
+                viewModel.getMovie(it)
+            } else {
+                isGetMovie(false)
+                viewModel.getMovie("")
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.dataFilm.debounce(300)
+                .distinctUntilChanged()
+                .collectLatest { list ->
+                    movieAdapter.submitData(list)
+                }
+        }
     }
 
+    private fun isGetMovie(isGet: Boolean) {
+        binding.apply {
+            if (isGet) {
+                tlGenre.visibility = View.GONE
+                pullToRefresh.visibility = View.GONE
+                rvList.visibility = View.VISIBLE
+            } else {
+                rvList.visibility = View.GONE
+                tlGenre.visibility = View.VISIBLE
+                pullToRefresh.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun navigateToDetailScreen(position: Int) {
+        movieAdapter.getItemByPosition(position)?.let {
+            navigate(DiscoverFragmentDirections.showDetails())
+        }
+    }
 }
--- app/src/main/java/com/example/cinemaddict/ui/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/example/cinemaddict/ui/discover/DiscoverViewModel.kt
@@ -3,18 +3,29 @@ package com.example.cinemaddict.ui.discover
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.cinemaddict.common.paging.MovPagingSource
+import com.example.cinemaddict.domain.entity.FilmDiscoverData
 import com.example.cinemaddict.domain.entity.GenreData
 import com.example.cinemaddict.domain.mapper.toGenreData
 import com.example.cinemaddict.domain.usecase.GenreUseCase
+import com.example.cinemaddict.domain.usecase.MovieForTitleUseCase
 import com.example.cinemaddict.network.ApiResponse
 import com.example.cinemaddict.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DiscoverViewModel @Inject constructor(
-    private val genreUseCase: GenreUseCase
+    private val genreUseCase: GenreUseCase,
+    private val movieUseCase: MovieForTitleUseCase
 ) : BaseViewModel() {
 
     private var _genre = MutableLiveData<List<GenreData>>()
@@ -23,8 +34,40 @@ class DiscoverViewModel @Inject constructor(
     private var _error = MutableLiveData<String>()
     val error: LiveData<String> = _error
 
+    val searchText = MutableLiveData<String>()
+
+    private var lastQuery: String = ""
+
+    private val _dataFilm = MutableStateFlow<PagingData<FilmDiscoverData>>(PagingData.empty())
+    val dataFilm: StateFlow<PagingData<FilmDiscoverData>> get() = _dataFilm
+
     init {
         getGenres()
+    }
+
+    fun getMovie(title: String) {
+        if (title != "") {
+            viewModelScope.launch {
+                Pager(
+                    config = PagingConfig(pageSize = 20),
+                    pagingSourceFactory = {
+                        MovPagingSource { page ->
+                            movieUseCase.getMovieForTitle(
+                                page = page,
+                                query = title
+                            )
+                        }
+                    }
+                ).flow
+                    .cachedIn(viewModelScope)
+                    .collectLatest { newData ->
+                        _dataFilm.value = newData
+                    }
+            }
+        } else {
+            _dataFilm.value = PagingData.empty()
+        }
+        lastQuery = title
     }
 
     private fun getGenres() {
@@ -47,6 +90,9 @@ class DiscoverViewModel @Inject constructor(
 
     fun refresh() {
         getGenres()
+        if (lastQuery.isNotEmpty()) {
+            getMovie(lastQuery)
+        }
     }
 
 }
